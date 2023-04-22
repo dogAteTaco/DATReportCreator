d30885141d042a7c50576e79545035dd3298cabb
eriml <eriml@DOGZEN>
2023-04-21 18:54:34 -0700

    Initial commit

---
+++ ReportCreator/ReportCreator/TestObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportCreator
+{
+	public class TestObject
+	{
+		[DisplayName("Identificador")]
+		public int id {  get; set; }
+		[DisplayName("Nombre")]
+		public string name { get; set; }
+		[DisplayName("Descripción")]
+		public string description { get; set; }
+		[DisplayName("Fecha")]
+		public DateTime created { get; set; }
+		[DisplayName("Precio")]
+		public float price { get; set; }
+		[DisplayName("Dato")]
+		public String dato { get; set; }
+		public TestObject(int id, string name, string description, float price, DateTime created)
+		{
+			this.id = id;
+			this.name = name;
+			this.description = description;
+			this.price = price;
+			this.created = created;
+		}
+	}
+}
---
+++ ReportCreator/ReportCreator/TestSingleFilterObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportCreator
+{
+	internal class TestSingleFilterObject
+	{
+		[DisplayName("Categoria")]
+		public int category { get; set; }
+
+		public List<TestObject> objects { get; set; }
+
+		public TestSingleFilterObject(int category, List<TestObject> objects)
+		{
+			this.category = category;
+			this.objects = objects;
+		}
+	}
+}
